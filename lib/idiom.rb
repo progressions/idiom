c305bcea1ecd82dd7da130cd36ba0a1c012ac1b6
Isaac Priestley <progressions@gmail.com>
2010-07-27 11:16:44 -0500

    Version bump to 0.0.9

--- lib/idiom.rb
+++ lib/idiom.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'rubygems'
 require 'active_support'
+require 'active_support/core_ext/hash'
+require 'fileutils'
 require 'rtranslate'
 require 'timer'
 require 'yrb'
